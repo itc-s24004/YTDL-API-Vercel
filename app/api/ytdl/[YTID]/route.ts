3db86b53bf96b3299ee6a954937b5892e4631e65
s24004 <s24004@std.it-college.ac.jp>
2025-03-04 18:10:11 +0900

    cookie

--- app/api/ytdl/[YTID]/route.ts
+++ app/api/ytdl/[YTID]/route.ts
@@ -40,29 +40,31 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
     try {
-        const cookies = await request.text()
+        const cookiesText = await request.text();
+
+        const cookies = JSON.parse(cookiesText).cookie;
 
         console.log(cookies);
-        return NextResponse.json([{url: "https://youtube.com"}], {status: 200});
+        // return NextResponse.json([{url: "https://youtube.com"}], {status: 200});
 
         // return NextResponse.json(reqJson);
-        // const url = new URL(request.url);
+        const url = new URL(request.url);
 
-        // const YTID = path.basename(url.pathname);
+        const YTID = path.basename(url.pathname);
 
-        // const info = await YoutubeDownloader.getInfo(YTID, cookies);
+        const info = await YoutubeDownloader.getInfo(YTID, cookies);
 
-        // // await YoutubeDownloader.downloadAudio(YTID)
+        // await YoutubeDownloader.downloadAudio(YTID)
 
-        // const audioFormats = info.formats.filter(data => data.hasAudio && !data.hasVideo && data.audioQuality == "AUDIO_QUALITY_MEDIUM")
+        const audioFormats = info.formats.filter(data => data.hasAudio && !data.hasVideo && data.audioQuality == "AUDIO_QUALITY_MEDIUM")
 
 
         
-        // // const params = url.searchParams;
-        // // const json = {YTID}
+        // const params = url.searchParams;
+        // const json = {YTID}
         
-        // // console.log(JSON.stringify(info, null, "    "));
-        // return NextResponse.json(audioFormats, { status: 200 });
+        // console.log(JSON.stringify(info, null, "    "));
+        return NextResponse.json(audioFormats, { status: 200 });
 
     } catch (error) {
         console.error(error)
